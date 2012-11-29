1f21c43714e25b921a096450db41f9bc8163d802
B-Fron <gandamiro@gmail.com>
2012-11-29 23:45:52 +0900

    제네릭스를 이용한 콜백 인터페이스 뭘 건드렸는지 아무것도 안되고 먹통이 되어서 여기에 걍 새로 올립니다

---
+++ number/src/springbook/learningtest/template/BufferedReaderCallback.java
@@ -0,0 +1,6 @@
+package springbook.learningtest.template;
+
+import java.io.*;
+public interface BufferedReaderCallback {
+	Integer doSomethingWithReader(BufferedReader br) throws IOException;
+}
---
+++ number/src/springbook/learningtest/template/CalcSumTest.java
@@ -0,0 +1,29 @@
+package springbook.learningtest.template;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class CalcSumTest {
+	Calculator calculator;
+	String numFilepath;
+	
+	@Before public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilepath = getClass().getResource("numbers.txt").getPath();
+	}
+	
+	@Test public void sumOfNumbers() throws IOException {
+		assertThat(calculator.calcSum(this.numFilepath), is(10));
+	}
+	@Test public void multiplayOfNumbers() throws IOException {
+		assertThat(calculator.calcMultiply(this.numFilepath), is(24));
+	}
+	@Test public void cancatenateStrings() throws IOException {
+		assertThat(calculator.concatenate(this.numFilepath), is("1234"));
+	}
+}
+//
